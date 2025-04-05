c9938208c146254a87d1c74f215a235b19035c82
FuRiousKidD <manhematakudzwa@gmail.com>
2025-04-06 01:31:17 +0200

    Added a realization of the priority queue which will use a heap data structure. Class added is PriorityQueueHeap<K,V>

---
+++ LeetLib/src/waste/men/datastructures/impl/hierarchical/PriorityQueueHeap.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package waste.men.datastructures.impl.hierarchical;
+
+import waste.men.datastructures.api.Entry;
+import waste.men.datastructures.util.abstractbase.AbstractPriorityQueueBase;
+
+/**
+ * A realization of the priority queue data structure, using a Heap Binary tree for
+ * its implementation
+ */
+public class PriorityQueueHeap<K,V> extends AbstractPriorityQueueBase<K, V> {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Entry<K, V> insert(K key, V value) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entry<K, V> min() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entry<K, V> removeMin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
